e6bf5b81051fa771a7f92ccf5083dcb707b46e8a
Magda_M <mg.matusiak@gmail.com>
2017-09-11 19:42:33 -0700

    plotly and selecting immune genes for ploting

--- Cibersort_res_analysis.R
+++ Cibersort_res_analysis.R
@@ -1,27 +1,60 @@
+mgsub <- function(pattern, replacement, x, ...) {
+  if (length(pattern)!=length(replacement)) {
+    stop("pattern and replacement do not have the same length.")
+  }
+  result <- x
+  for (i in 1:length(pattern)) {
+    result <- gsub(pattern[i], replacement[i], result, ...)
+  }
+  result
+}
+
+
 #############
 path = "/home/magda/Desktop/cibersortX/run2/CV_filtered"
 setwd(path)
 files <- list.files(path=path)
 
+#extract names
+namesGEP = as.character()
+for (i in files){
+  nam = mgsub(c("CIBERSORTxGEP_bulk_","_unames.txt_GEPs_CVFilteredAll.txt"),c("",""),i)
+  namesGEP = c(namesGEP, nam)
+}
+
+#put all files in the lyst
+filteredGEP = list()
+for (i in 1: length(files)){
+  r = read.table(files[i], sep = "\t", header = TRUE)
+  filteredGEP[[i]] = r
+}
+names(filteredGEP) = namesGEP
+
+gene_count = data.frame()
+for(i in namesGEP){
+  gene_count = rbind(gene_count, apply(filteredGEP[[i]], 2, function(x){sum(!is.na(x))}))
+}
+
+rownames(gene_count) = namesGEP
+colnames(gene_count) =colnames(filteredGEP[[1]])
+
 #extract all gene names from the files and put them in the list to find intersect 
 #of genes acros all the histological subtypes
 results = list()
-for (i in 1: length(files)){
-  r = read.table(files[i], sep = "\t", header = TRUE)
-  results[[i]] = as.character(r[,1])
+for (i in namesGEP){
+  results[[i]] = as.character(filteredGEP[[i]][,1])
 }
 
 universe = Reduce(intersect, results)
 
 #extract genes in monocyte signature common for all samples 
 Mono_ex = data.frame(matrix(NA, nrow = 9748, ncol = 1))
-for (i in 1: length(files)){
-  r = read.table(files[i], sep = "\t", header = TRUE)
-  Mono = r[r[,1] %in% universe,c(1,6)] #select gene_names and Monocytes
+for (i in namesGEP){
+  Mono = filteredGEP[[i]][filteredGEP[[i]][,1] %in% universe,c(1,6)] #select gene_names and Monocytes
   Mono_ex = cbind(Mono_ex,Mono)
 }
 
-all(as.character(Mono_ex[,2])==as.character(Mono_ex[,4]))
+all(as.character(Mono_ex[,10])==as.character(Mono_ex[,4]))
 rownames(Mono_ex) = as.character(Mono_ex[,2])
 Mono_ex = Mono_ex[, seq(3, length(Mono_ex), 2)]
 colnames(Mono_ex) = c("AVL_Mono","DCIS_Mono","EN_Mono","IDC_Mono","LN_Mono","MetECE_Mono","normal_Mono")
@@ -30,31 +63,61 @@ Mono_ex[Mono_ex<=1] <- NA
 predicted_genes_Monocytes = apply(Mono_ex, 2, function(x){sum(!is.na(x))})
 write.table(predicted_genes_Monocytes, "predicted_genes_Monocytes_in_stages.txt", col.names = FALSE, quote = FALSE)
 
-Mono_ex_DCIS = Mono_ex[!is.na(Mono_ex$DCIS_Mono),] 
-cor(Mono_ex,use="pairwise.complete.obs",method="pearson") # can't use spearman here
-al3_mono = Mono_ex[rowSums(!is.na(Mono_ex)) >= 3,]
+#Mono_ex_DCIS = Mono_ex[!is.na(Mono_ex$DCIS_Mono),] 
+#cor(Mono_ex,use="pairwise.complete.obs",method="pearson") # can't use spearman here
+al2_mono = Mono_ex[rowSums(!is.na(Mono_ex)) >= 2,]
 
-###########
+
+#############
+#heatmaps
+#############
+
+EN_DCIS_genes = change1$gene.names[c(1:100,1313:1412)]
+to_heatmap = change[change$gene.names %in% EN_DCIS_genes,c(1,4,5,9)]
+to_heatmap2 = to_heatmap[order(to_heatmap$EN_DCIS, decreasing = TRUE),]
+rownames(to_heatmap2) = to_heatmap2[,1]
+to_heatmap2 = to_heatmap2[,-1]
+library(heatmaply)
+heatmaply(log2(to_heatmap2[,c(1,2)]+1), dendrogram = "row")
+
+##########
 #plot gene values in different stages 
 ###########
-
+library(plotly)
 library(reshape2)
 library(dplyr)
-to_reshape = data.frame(gene.names = rownames(al3_mono), al3_mono)
+to_reshape = data.frame(gene.names = rownames(al2_mono), al2_mono)
 to_reshape = to_reshape[,c(1,8,6,4,3,5,2,7)]
-change = mutate(to_reshape, EN_DCIS = log2(to_reshape$DCIS_Mono/to_reshape$EN_Mono), 
-                DCIS_IDC = log2(to_reshape$IDC_Mono/to_reshape$DCIS_Mono))
 
+#compute log2 Fold Changes
+change = mutate(to_reshape, EN_DCIS = log2(to_reshape$DCIS_Mono/to_reshape$EN_Mono), 
+                DCIS_IDC = log2(to_reshape$IDC_Mono/to_reshape$DCIS_Mono),EN_IDC = log2(to_reshape$IDC_Mono/to_reshape$EN_Mono))
+
+ImGenes_HLA_CTSB = c("CTSB","HLA.DMA","HLA.DPB1","HLA.DRB1","HLA.DQB1")
+ImGenes_rest = c("TGFB1","TGFB2","CSF1R","MUC4","TNFSF12","TNFRSF1A","TRADD","APOE",
+                 "CD68","CD163","CD47","CD14","CD80","CD86")
+ImGenes_NFkB_IFN = c("NFKB1A","MYD88","TLR3","IFNAR1","IFNAR2","IFNGR1","IFNGR2","STAT1")
+Immuno = change[change$gene.names %in% ImGenes_NFkB_IFN,]
+Immuno = Immuno[,c(1,4,5,6)]
+long_Imm = melt(Immuno, id.vars = "gene.names")
+p <- plot_ly(long_Imm, x = ~variable, y = ~value, color = ~gene.names, text = ~gene.names) %>%
+  add_lines() %>% add_annotations(long_Imm$gene.names) %>% layout(showlegend = FALSE)  
+
+
+
+#select genes with biggest FC difference between EN and DCIS
 change1 = change[order(change$EN_DCIS, decreasing = TRUE),c(1,9,10)]
 To_GOEA_up_EN_DCIS = change1$gene.names[which(change1$EN_DCIS >= 0.5)]
 To_GOEA_down_EN_DCIS = change1$gene.names[which(change1$EN_DCIS <= -1)]
 
+##select genes with biggest FC difference between DCIS and IDC
 change2 = change[order(change$DCIS_IDC, decreasing = TRUE),c(1,9,10)]
 To_GOEA_up_DCIS_IDC = change2$gene.names[which(change2$DCIS_IDC >= 1)]
 To_GOEA_down_DCIS_IDC = change2$gene.names[which(change2$DCIS_IDC <= -0.8)]
 
 scaled_up_EN_DCIS = to_reshape[to_reshape$gene.names %in% To_GOEA_up_EN_DCIS,c(1,4,5)]
 scaled_up_EN_DCIS = data.frame(gene.names = scaled_up_EN_DCIS[,1], scaled_up_EN_DCIS[,c(2,3)]/scaled_up_EN_DCIS[,2])
+scaled_up_EN_DCIS = scaled_up_EN_DCIS[scaled_up_EN_DCIS$,]
 long_scaled_up_EN_DCIS = melt(scaled_up_EN_DCIS,id.vars = "gene.names")
 p <- plot_ly(long_scaled_up_EN_DCIS, x = ~variable, y = ~value, color = ~gene.names, text = ~gene.names) %>%
   add_lines() %>% add_annotations(long_scaled_up_EN_DCIS$gene.names) %>% layout(showlegend = FALSE)  
@@ -62,13 +125,7 @@ p <- plot_ly(long_scaled_up_EN_DCIS, x = ~variable, y = ~value, color = ~gene.na
 
 scaled_down_EN_DCIS
 
-# EN_DCIS_genes = change1$gene.names[c(1:100,1313:1412)]
-# to_heatmap = change[change$gene.names %in% EN_DCIS_genes,c(1,4,5,9)]
-# to_heatmap2 = to_heatmap[order(to_heatmap$EN_DCIS, decreasing = TRUE),]
-# rownames(to_heatmap2) = to_heatmap2[,1]
-# to_heatmap2 = to_heatmap2[,-1]
-# library(heatmaply)
-# heatmaply(log2(to_heatmap2[,c(1,2)]+1), dendrogram = "row")
+
 
 long_Mono = melt(to_reshape, id.vars = "gene.names")
 p <- plot_ly(long_Mono, x = ~variable, y = ~value, color = ~gene.names, text = ~gene.names) %>%
